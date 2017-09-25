1fe73ab02091b6fad23eb2e51300fd3491d71b57
SiGm4 <dimgmx@hotmail.com>
2017-09-25 17:26:40 +0300

    Separated navbar

---
+++ navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-fixed-top">
+    <div class="container-fluid" id="nav-container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>                        
+            </button>
+            <a class="navbar-brand" href="index.html">
+                <img class="logo-responsive" id="logo" src="basic_logo.png" alt="Morning Pages Logo"></img>
+            </a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="index.html">HOME</a></li>
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="#">CONTACT</a></li>
+                <li><a href="signup.php">SIGN UP</a></li>
+                <li><a href="login.php">LOGIN</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
